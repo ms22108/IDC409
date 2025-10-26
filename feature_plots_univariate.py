342e07f1ba0a5087e1ea0cc59736b587e064781b
Ms22237 <ms22237@iisermohali.ac.in>
2025-10-26 16:20:55 +0530

    Plot Features

--- feature_plots_univariate.py
+++ feature_plots_univariate.py
@@ -1,2 +1,4 @@
 feature_dist('cosTBTO', both=True, x_axis_lim=[0,1])
 feature_dist('cosTBz', both=True, x_axis_lim=[0,1])
+feature_dist('thrustBm', 'Thrust feature', both=True)
+feature_dist('thrustOm', 'Thrust feature', both=True)
