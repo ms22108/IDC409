8bfe6fcc09f1c745b06029fc5bd15aa75a8ee687
Ms22237 <ms22237@iisermohali.ac.in>
2025-10-26 16:30:09 +0530

    Corr Matrix

--- feature_reduc.py
+++ feature_reduc.py
@@ -10,3 +10,10 @@ df['is_signal'] = df['type'].apply(lambda x: 1 if x == 0 or x == 1 else 0)
 feature_columns = df.select_dtypes(include=np.number).drop(columns=['type', 'is_signal', 'Unnamed: 0'])
 corr_matrix = feature_columns.corr().abs()
 
+sns.set(font_scale=0.9)
+
+# plot for correlation matrix
+plt.figure(figsize=(15, 15))
+sns.heatmap(corr_matrix, cmap='magma', annot=False)
+
+
