898a7186bea3ec1b84c0aabe419fdc43d7cc5ae8
PMishra04 <139467942+PMishra04@users.noreply.github.com>
2025-10-26 17:04:01 +0530

    scaling

--- log_reg.py
+++ log_reg.py
@@ -5,3 +5,12 @@ from sklearn.metrics import roc_curve, roc_auc_score, confusion_matrix
 
 y = df_reduced['is_signal']
 X = df_reduced.drop(columns=['type', 'is_signal', 'Unnamed: 0'])
+
+# scale features
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+X_train, X_test, y_train, y_test = train_test_split(
+    X_scaled, y, # Use the scaled data
+    test_size=0.3,
+    random_state=42,
+    stratify=y)
