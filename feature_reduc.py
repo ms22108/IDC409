de841d5cebdeabb18c521dbdbed925942be16c33
PMishra04 <139467942+PMishra04@users.noreply.github.com>
2025-10-26 16:26:32 +0530

    feature reduction

---
+++ feature_reduc.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# load data
+df = pd.read_csv("data_hep.csv")
+df['is_signal'] = df['type'].apply(lambda x: 1 if x == 0 or x == 1 else 0)
+
+feature_columns = df.select_dtypes(include=np.number).drop(columns=['type', 'is_signal', 'Unnamed: 0'])
+corr_matrix = feature_columns.corr().abs()
+
