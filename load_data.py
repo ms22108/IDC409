6b6fec42690a46a1a144d80c0390657252656d75
Ms22237 <ms22237@iisermohali.ac.in>
2025-10-26 16:02:53 +0530

    visualizing dist

--- load_data.py
+++ load_data.py
@@ -6,3 +6,9 @@ import seaborn as sns
 # load data
 df = pd.read_csv("data_hep.csv")
 
+# visualising distribution of event types
+print(df['type'].value_counts())
+plt.figure(figsize=(4, 2))
+sns.countplot(x='type', data=df)
+plt.title('Distribution of Event Types')
+plt.show()
