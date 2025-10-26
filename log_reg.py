ffb6556ff730ace0509120d93d025760622fb4cd
PMishra04 <139467942+PMishra04@users.noreply.github.com>
2025-10-26 17:06:16 +0530

    making combined df

--- log_reg.py
+++ log_reg.py
@@ -25,3 +25,9 @@ log_reg_classifier = LogisticRegression(
 log_reg_classifier.fit(X_train, y_train)
 
 y_pred_proba = log_reg_classifier.predict_proba(X_test)[:, 1]
+
+# combine results into a dataframe for plotting
+results_df = pd.DataFrame({'true_label': y_test, 'logreg_score': y_pred_proba})
+signal_scores = results_df[results_df['true_label'] == 1]['logreg_score']
+background_scores = results_df[results_df['true_label'] == 0]['logreg_score']
+
