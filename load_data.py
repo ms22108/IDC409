e67883645597bfd9ad1a4591cfa68620cd4795ad
PMishra04 <139467942+PMishra04@users.noreply.github.com>
2025-10-26 16:04:53 +0530

    signal vs bg

--- load_data.py
+++ load_data.py
@@ -12,3 +12,11 @@ plt.figure(figsize=(4, 2))
 sns.countplot(x='type', data=df)
 plt.title('Distribution of Event Types')
 plt.show()
+
+# we want 0 and 1 types as signal and rest as background
+df['is_signal'] = df['type'].apply(lambda x: 1 if x == 0 or x == 1 else 0) # signal is 1, rest all are background
+print(df['is_signal'].value_counts())
+plt.figure(figsize=(2, 2))
+sns.countplot(x='is_signal', data=df)
+plt.title("Binary classification: signal:1")
+plt.show()
