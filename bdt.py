0db655cfab130a2bcc01aa6d0070aa48a322f788
Ms22237 <ms22237@iisermohali.ac.in>
2025-10-27 11:58:37 +0530

    bdt

--- bdt.py
+++ bdt.py
@@ -15,3 +15,13 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 print(f"Training set size: {len(X_train)} events")
 print(f"Testing set size:  {len(X_test)} events")
 
+# LightGMB model
+lgbm_classifier = lgb.LGBMClassifier(objective='binary', n_estimators=1000, learning_rate=0.05, num_leaves=31, random_state=42)
+
+# Training
+lgbm_classifier.fit(X_train, y_train, eval_set=[(X_test, y_test)], eval_metric='auc', callbacks=[lgb.early_stopping(100, verbose=True)])
+
+# plotting classifier output distribution
+print("plotting classifier output distribution")
+import matplotlib
+import matplotlib.pyplot as plt
